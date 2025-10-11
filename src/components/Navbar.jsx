7029ba1d8141c287fea7007527b7e5f0710a0b28
Sajid Raza <sajidsidd30@gmail.com>
2025-10-11 14:12:19 +0530

    Your commit message here

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,26 @@
+import { useState } from 'react';
 import '../Navbar.css';
 
 export default function Navbar({ setActiveSection }) {
-    return (
-        <nav className="navbar">
-            <div className="navbar-left">
-                <img src="/logo.png" alt="Logo" className="navbar-logo" />
-                <span className="site-title">CourseRec</span>
-            </div>
-            <ul className="navbar-links">
-                <ul className="navbar-links">
-                    <li><a href="/" onClick={() => setActiveSection("home")}>Home</a></li>
-                    <li><a href="#" onClick={() => setActiveSection("courses")}>Courses</a></li>
-                    <li><a href="#" onClick={() => setActiveSection("about")}>About</a></li>
-                    <li><a href="#" onClick={() => setActiveSection("contact")}>Contact</a></li>
-                </ul>
-            </ul>
-        </nav>
-    );
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  return (
+    <nav className="navbar">
+      <div className="navbar-left">
+        <img src="/logo.png" alt="Logo" className="navbar-logo" />
+        <span className="site-title">Course-Recommendation</span>
+      </div>
+
+      <button className="menu-toggle" onClick={() => setMenuOpen(!menuOpen)}>
+        ☰
+      </button>
+
+      <ul className={`navbar-links ${menuOpen ? 'open' : ''}`}>
+        <li><a href="/" onClick={() => setActiveSection("home")}>Home</a></li>
+        <li><a href="#" onClick={() => setActiveSection("courses")}>Courses</a></li>
+        <li><a href="#" onClick={() => setActiveSection("about")}>About</a></li>
+        <li><a href="#" onClick={() => setActiveSection("contact")}>Contact</a></li>
+      </ul>
+    </nav>
+  );
 }
